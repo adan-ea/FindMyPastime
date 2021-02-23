f22933a79d5173f435feba5d90b8d1e4f587c6d7
adan-ea <57812567+adan-ea@users.noreply.github.com>
2021-02-23 16:23:12 +0100

    Ajout d'une activité

--- app/src/main/java/com/example/findmypastime/MainActivity.java
+++ app/src/main/java/com/example/findmypastime/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.findmypastime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonCamera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.buttonCamera = findViewById(R.id.buttonCamera);
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraActivity = new Intent(getApplicationContext(), CameraActivity.class);
+                finish();
+            }
+        });
     }
 }
