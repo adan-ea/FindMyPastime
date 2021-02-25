7147b82c916c44703b1574e36a1b24d670231c31
adan-ea <57812567+adan-ea@users.noreply.github.com>
2021-02-25 05:32:57 +0100

    LA CAMERA FONCTIONNE SA MERE

--- app/src/main/java/com/example/findmypastime/MainActivity.java
+++ app/src/main/java/com/example/findmypastime/MainActivity.java
@@ -1,29 +1,63 @@
 package com.example.findmypastime;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonCamera;
+    //initialisation des variables
+    ImageView imageView;
+    Button btn_camera;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.buttonCamera = findViewById(R.id.buttonCamera);
+        //Assigner les variables
+        imageView = findViewById(R.id.imgAffichePhoto);
+        btn_camera = findViewById(R.id.btn_camera);
 
-        buttonCamera.setOnClickListener(new View.OnClickListener() {
+        //Demander la persmission pour la caméra
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    }, 100);
+        }
+        btn_camera.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                Intent cameraActivity = new Intent(getApplicationContext(), CameraActivity.class);
-                startActivity(cameraActivity);
-                finish();
+            public void onClick(View view){
+                //Ouvre la caméra
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+startActivityForResult(intent, 100);
             }
         });
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+       if(requestCode == 100){
+           //Recupère l'image capturée
+           Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+           //Prend l'image récupérée et la met dans le placeHolder
+           imageView.setImageBitmap(captureImage);
+
+       }
     }
 }
